562a3db395f5e927d28575e2e5b8f4cfe81b7b93
virmoc <virmoc411@gmail.com>
2022-01-27 12:29:09 +0100

    dolgozo adatbetöltés kezdetleges

--- js/dolgozo.js
+++ js/dolgozo.js
@@ -1,3 +1,24 @@
 class Dolgozo{
-    
+    constructor(node, adat) {
+        this.node = node;
+        /**selectorok elnvezése */
+        this.adat = adat;
+        this.dolgNev = this.node.children(".dolgozonev");
+        this.szulDatum = this.node.children(".szuldatum");
+        this.dolgCim = this.node.children(".cime");
+        this.dolgTel = this.node.children(".telszam");
+        this.dolgMunka = this.node.children(".munkakor");
+        this.dolgJelszo = this.node.children(".jelszo");
+
+    }
+
+    setAdat(adat) {
+        this.adat = adat;
+        this.dolgNev.text(adat.neve);
+        this.szulDatum.text(adat.szuldatum);
+        this.dolgCim.text(adat.cim);
+        this.dolgTel.text(adat.telefonszam);
+        this.dolgMunka.text(adat.munkakor_id);
+        this.dolgJelszo.text(adat.jelszo);
+    }
 }
---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function () {
+    const myAjax = new MyAjax;
+    const dolgozoTomb = [];
+    let apivegpont="http://localhost:3000/dolgozok";
+    const szuloElem = $(".dolgozok");
+    
+    myAjax.adatBetolt(apivegpont, dolgozoTomb, dolgozoKiir);
+
+    function dolgozoKiir(dolgozok){
+        const sablonElem = $('tbody .termek');
+        szuloElem.empty();
+        sablonElem.show();
+        
+
+        dolgozok.forEach(function(elem) {
+            let node = sablonElem.clone().appendTo(szuloElem);
+            const obj = new Dolgozo(node, elem);
+
+        });
+        sablonElem.hide();
+    }
+
+});
