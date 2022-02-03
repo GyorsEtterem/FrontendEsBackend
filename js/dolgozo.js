68c477a2748c4750fa2a14be211c78bb6dce126b
virmoc <virmoc411@gmail.com>
2022-02-03 14:54:42 +0100

    reszponzivitas_dolgozoAdatbeolv

--- js/dolgozo.js
+++ js/dolgozo.js
@@ -1,5 +1,6 @@
 class Dolgozo{
     constructor(node, adat) {
+        console.log(adat)
         this.node = node;
         /**selectorok elnvezése */
         this.adat = adat;
@@ -7,18 +8,20 @@ class Dolgozo{
         this.szulDatum = this.node.children(".szuldatum");
         this.dolgCim = this.node.children(".cime");
         this.dolgTel = this.node.children(".telszam");
-        this.dolgMunka = this.node.children(".munkakor");
         this.dolgJelszo = this.node.children(".jelszo");
+        this.dolgMunka = this.node.children(".munkakor");
+        this.setAdat(this.adat);
 
     }
 
     setAdat(adat) {
+        console.log(adat)
         this.adat = adat;
         this.dolgNev.text(adat.neve);
         this.szulDatum.text(adat.szuldatum);
         this.dolgCim.text(adat.cim);
         this.dolgTel.text(adat.telefonszam);
-        this.dolgMunka.text(adat.munkakor_id);
         this.dolgJelszo.text(adat.jelszo);
+        this.dolgMunka.text(adat.munkakor_id);
     }
 }
--- js/script.js
+++ js/script.js
@@ -2,22 +2,25 @@ $(function () {
     const myAjax = new MyAjax;
     const dolgozoTomb = [];
     let apivegpont="http://localhost:3000/dolgozok";
-    const szuloElem = $(".dolgozok");
+    const szuloElem = $(".dolgozok_tabla");
     
     myAjax.adatBetolt(apivegpont, dolgozoTomb, dolgozoKiir);
 
     function dolgozoKiir(dolgozok){
-        const sablonElem = $('.termek');
+        console.log(dolgozok)
+        const sablonElem = $('.dolgozo');
         szuloElem.empty();
         sablonElem.show();
         
 
         dolgozok.forEach(function(elem) {
+            console.log(dolgozok)
             let node = sablonElem.clone().appendTo(szuloElem);
             const obj = new Dolgozo(node, elem);
+            
 
         });
-        //sablonElem.hide();
+        sablonElem.hide();
     }
 
 });
