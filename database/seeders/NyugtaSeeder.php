9f995660c3c2c3f9447f600d9dd8b279c8f1c374
virmoc <virmoc411@gmail.com>
2022-03-31 18:51:59 +0200

    seederek javítása

--- database/seeders/NyugtaSeeder.php
+++ database/seeders/NyugtaSeeder.php
@@ -15,11 +15,11 @@ class NyugtaSeeder extends Seeder
     public function run()
     {
         DB::table('nyugtas')->insert([
-            ['nyugtaszam' =>'700', 'vegsoOsszeg' => 5 , 'fizetesAllapot' => true, 'fizetesMod' => true,'datumrend' =>'2022-03-23','datumkifizet' =>'2022-03-23', 'dolg_id' => 1, 'megjegyzes' =>'nincs'],
-            ['nyugtaszam' =>'701', 'vegsoOsszeg' => 5 , 'fizetesAllapot' => true, 'fizetesMod' => false,'datumrend' =>'2022-03-23','datumkifizet' =>'2022-03-23','dolg_id'=> 1, 'megjegyzes' => 'nincs'],
-            ['nyugtaszam' =>'702', 'vegsoOsszeg' => 3 , 'fizetesAllapot' => true, 'fizetesMod' => true,'datumrend' =>'2022-03-24', 'datumkifizet' =>'2022-03-24', 'dolg_id' =>3, 'megjegyzes' =>'nincs'],
-            ['nyugtaszam' =>'703', 'vegsoOsszeg' => 1 , 'fizetesAllapot' => true, 'fizetesMod' => false,'datumrend' =>'2022-03-25','datumkifizet' =>'2022-03-25', 'dolg_id' => 1, 'megjegyzes' =>'nincs'],
-            ['nyugtaszam' =>'704', 'vegsoOsszeg' => 2 , 'fizetesAllapot' => true, 'fizetesMod' => true,'datumrend' =>'2022-03-25','datumkifizet' =>'2022-03-25', 'dolg_id' => 2, 'megjegyzes' =>'nincs']
+            ['nyugtaszam' =>'700', 'vegsoOsszeg' => 5 , 'fizetesAllapot' => true, 'fizetesMod' => true,'datumrend' =>'2022-03-23','datumkifizet' =>'2022-03-23',  'megjegyzes' =>'nincs'],
+            ['nyugtaszam' =>'701', 'vegsoOsszeg' => 5 , 'fizetesAllapot' => true, 'fizetesMod' => false,'datumrend' =>'2022-03-23','datumkifizet' =>'2022-03-23', 'megjegyzes' => 'nincs'],
+            ['nyugtaszam' =>'702', 'vegsoOsszeg' => 3 , 'fizetesAllapot' => true, 'fizetesMod' => true,'datumrend' =>'2022-03-24', 'datumkifizet' =>'2022-03-24',  'megjegyzes' =>'nincs'],
+            ['nyugtaszam' =>'703', 'vegsoOsszeg' => 1 , 'fizetesAllapot' => true, 'fizetesMod' => false,'datumrend' =>'2022-03-25','datumkifizet' =>'2022-03-25',  'megjegyzes' =>'nincs'],
+            ['nyugtaszam' =>'704', 'vegsoOsszeg' => 2 , 'fizetesAllapot' => true, 'fizetesMod' => true,'datumrend' =>'2022-03-25','datumkifizet' =>'2022-03-25',  'megjegyzes' =>'nincs']
         ]);
     }
 }
